363f87d322ef8584dbb17c2b05b80c219c0a9f3d
Ivy Kabere <kabereivy@gmail.com>
2022-04-24 11:31:57 +0300

    add setUp method and create new instance of user)

--- user_test.py
+++ user_test.py
@@ -24,6 +24,14 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.username, "Mary")
         self.assertEqual(self.new_user.password, "Mario123")
 
+    def test_save_user(self):
+        '''
+             test_save_user test case to test if the user object is saved into
+              the user list
+             '''
+        self.new_user.save_user()  # saving the new user details
+        self.assertEqual(len(User.user_list), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
