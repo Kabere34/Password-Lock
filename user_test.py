3b2b56af5024988e97e93b8942d851b4c096b6f6
Ivy Kabere <kabereivy@gmail.com>
2022-04-25 00:07:23 +0300

    create a class that returns credentials_list)

--- user_test.py
+++ user_test.py
@@ -112,6 +112,14 @@ class TestCredentials(unittest.TestCase):
         credentials_exists = Credentials.credentials_exist("Twitter")
         self.assertTrue(credentials_exists)
 
+    def test_display_all_credentials(self):
+        '''
+        method that returns a list of all credentials saved
+        '''
+
+        self.assertEqual(Credentials.display_credentials(),
+                         Credentials.credentials_list)
+
 
 if __name__ == '__main__':
     unittest.main()
