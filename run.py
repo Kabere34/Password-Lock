64f56da67884af8729be2f99da04fb94008abd3e
Ivy Kabere <kabereivy@gmail.com>
2022-04-25 01:14:13 +0300

    create a function create_new_user

--- run.py
+++ run.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python3.10.4
 from user import User, Credentials
+
+
+def create_new_user(username, password):
+    '''
+    Function to create a new user
+    '''
+
+    new_user = User(username, password)
+    return new_user
