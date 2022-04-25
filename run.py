2f4d3c96627d69399fbcdef1fd92e3e6abbbc0ec
Ivy Kabere <kabereivy@gmail.com>
2022-04-25 09:28:56 +0300

    create a function to save credentials

--- run.py
+++ run.py
@@ -16,3 +16,35 @@ def save_user(user):
       Function to save user
     '''
     user.save_user()
+
+
+def display_user():
+    '''
+    Function to display existing user
+    '''
+
+    return User.display_user()
+
+
+def login_user(username, password):
+    """
+        function that checks whether a user exist and then login the user in.
+    """
+
+    check_user = Credentials.verify_user(username, password)
+    return check_user
+
+
+def create_new_credentials(account, username, password):
+    """
+        Function that creates new credentials for a given user account
+    """
+    new_credentials = Credentials(account, username, password)
+    return new_credentials
+
+
+def save_credentials(credentials):
+    '''
+    Function to save Credentials to the credentials list
+    '''
+    credentials.save_credentials()
